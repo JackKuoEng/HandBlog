5a9f0eae1059cfdd1d6acbadd90407f2dcd93fe8
Jack.J.Kuo <jack.j.kuo0817@gmail.com>
2022-08-09 11:11:14 +0800

    fix bug

--- handblog/main/route.py
+++ handblog/main/route.py
@@ -8,9 +8,15 @@ main = Blueprint('main', __name__)
 @main.route("/")
 @main.route("/home")
 def home():
+    if 'keyword' in request.args:
+        keyword = request.args['keyword']
+        page = request.args.get('page', 1 , type=int)
+        # search the posts using the keyword
+        posts = Posts.query.filter(Posts.title.like(f'%{keyword}%')).order_by(Posts.date_posted.desc()).paginate(page=page, per_page=5)
     # 從資料庫獲取文章
-    page = request.args.get('page', 1 , type=int)
-    posts = Posts.query.order_by(Posts.date_posted.desc()).paginate(page=page, per_page=5)
+    else:
+        page = request.args.get('page', 1 , type=int)
+        posts = Posts.query.order_by(Posts.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template("posts.html", posts=posts)
 
 @main.context_processor
